f7d8bc39a6ea7d7673d3bd85a368c9de3947f274
Coollbreackerz <ralberinfante4@gmail.com>
2023-11-22 15:58:43 -0400

    Deque Succes

---
+++ Deque.cs
@@ -0,0 +1,38 @@
+﻿
+
+public class Deque<T>
+{
+    public Deque()
+    {
+        _Train = new();
+    }
+    public void Push(T value)
+    {
+        _Train.Add(value);
+    }
+
+    public T Pop()
+    {
+        var value = _Train[^1];
+
+        _Train.Remove(value);
+
+        return value;
+    }
+
+    public void Unshift(T value)
+    {
+        _Train.Insert(0, value);
+    }
+
+    public T Shift()
+    {
+        var value = _Train[0];
+
+        _Train.Remove(value);
+
+        return value;
+    }
+
+    private List<T> _Train;
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine();
+﻿
+
+Console.WriteLine();
+
