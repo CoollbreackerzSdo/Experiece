0ec5ac1ed0af583043e4b43c9c3a104ca567aee7
Coollbreackerz <ralberinfante4@gmail.com>
2023-12-08 13:03:04 -0400

    Isogram Complete

---
+++ Pangram.cs
@@ -0,0 +1,17 @@
+﻿public static class Pangram
+{
+    public static bool IsPangram(string input)
+        => string.IsNullOrEmpty(input) ? false : IsEnglishPangram(input.ToLower());
+    private static bool IsEnglishPangram(ReadOnlySpan<char> input)
+    {
+        var buffer = new HashSet<char>();
+
+        foreach (var item in input)
+        {
+            if (!char.IsLetter(item)) continue;
+            buffer.Add(item);
+        }
+
+        return buffer.Count == 26;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿Console.WriteLine(char.IsLetter('3'));
-Console.WriteLine();
+﻿using System.Diagnostics;
+
+var timer = new Stopwatch();
+timer.Start();
+Console.WriteLine(Isogram.IsIsogram("isogram"));
+timer.Stop();
+Console.WriteLine($"Time:{timer.Elapsed.TotalMilliseconds}ms");
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        if (string.IsNullOrEmpty(word)) return true;
+        {
+            word = word.ToLower();
+
+            var elements = new HashSet<char>();
+
+            foreach (var item in word)
+            {
+                if (item == ' ' || item == '-') continue;
+
+                else if (elements.Contains(item)) return false;
+
+                elements.Add(item);
+            }
+
+            return true;
+        }
+    }
+}
