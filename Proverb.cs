c02ba809d6c58be4b3921b8af2f94cb74c508680
CoollbreackerzSdo <ralberinfante4@gmail.com>
2024-10-11 21:00:21 -0400

    Proverb complete

---
+++ Proverb.cs
@@ -0,0 +1,8 @@
+﻿public static class Proverb
+{
+    public static string[] Recite(string[] subjects)
+        => subjects.Length == 0 ? Array.Empty<string>()
+        : subjects.Zip(subjects.Skip(1))
+            .Select(compresionPair => $"For want of a {compresionPair.First} the {compresionPair.Second} was lost.")
+            .Append($"And all for the want of a {subjects.First()}.").ToArray();
+}
